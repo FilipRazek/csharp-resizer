6deef0bf7d091c169034737c77851ee0851e2116
Filip Razek <filip.razek@protonmail.com>
2024-03-19 14:01:48 +0100

    Add Person class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Person person = new ("John", 25);
+        Console.WriteLine(person.Hello(true));
+    }
+
+}
+
+
+class Person(string name, int age)
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+
+
+    public string Hello(bool isLowercase)
+    {
+        string message = $"hello {Name}, you are {Age}";
+        return isLowercase ? message.ToLower() : message.ToUpper();
     }
 
 }
